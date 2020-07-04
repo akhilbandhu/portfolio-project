1de4b8119fd923c30d7419586f2bea01f9db1b82
Akhil <akhilbandhu@gmail.com>
2020-07-04 16:54:57 -0700

    Added resume, blogs and static files folder

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     title = models.CharField(max_length = 255)
+
+    def __str__(self):              #shows title of blog in database instead of blog object
+        return self.title
+
+    def summary(self):              #doesn't show whole blog, first 100 characters
+        return self.body[:100]
+
+    def pub_date_pretty(self):      #makes date pretty, look up strftime references
+        return self.pub_date.strftime( '%b %e %Y' )
