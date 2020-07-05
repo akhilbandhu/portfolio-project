7ec5e66a547f46b2c28d38f8666af4da7d452e03
Akhil <akhilbandhu@gmail.com>
2020-07-05 10:25:25 -0700

    Security enhanced

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8w0ugwd1@hb4$-*-f!9_i0-1&xksu5^gjyael10tpn7bedo4@c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
